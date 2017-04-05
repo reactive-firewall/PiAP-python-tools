094d5f1aa6dfcfc802cb1f0f4dcb8819e8fdc937
reactive-firewall <reactive-firewall@users.noreply.github.com>
2017-04-04 20:52:25 -0700

    made the tests less hypocritical

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 # Pocket PiAP
-# 
+# ..................................
 # Copyright (c) 2017, Kendrick Walls
-# 
+# ..................................
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+# ..........................................
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+# ..........................................
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,6 +30,8 @@ except Exception as ImportErr:
 
 try:
 	from tests import test_basic
+	if test_basic.__name__ is None:
+		raise ImportError(u'Test module failed to import even the basic tests.')
 except Exception as importErr:
 	importErr = None
 	del importErr
