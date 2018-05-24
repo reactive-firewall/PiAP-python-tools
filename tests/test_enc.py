2fe709d9264d5c0b1264739ffaae565e5062afce
reactive-firewall <reactive-firewall@users.noreply.github.com>
2018-05-23 23:20:26 -0700

    code style fix for last change

--- tests/test_enc.py
+++ tests/test_enc.py
@@ -417,7 +417,12 @@ class CryptoTestSuite(unittest.TestCase):
 				raise unittest.SkipTest("BETA. Experemental feature not ready yet.")
 		assert theResult
 
-	@unittest.skipUnless(((sys.version_info > (3, 2)) or (sys.getdefaultencoding() in 'utf-8')), sub_proc_bug_message)
+	@unittest.skipUnless(
+		(
+			(sys.version_info > (3, 2)) and (sys.getdefaultencoding() in 'utf-8')
+		),
+		sub_proc_bug_message
+	)
 	@given(text())
 	def test_case_clarify_write_inverts_read(self, someInput):  # noqa C901
 		"""Tests the write then read workflow of keyring.clarify with fuzzing."""
