bc60be40d895c05b9457a869087ea0c675e0e758
reactive-firewall <reactive-firewall@users.noreply.github.com>
2018-07-14 22:39:09 -0700

    version bump

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ except Exception:
 
 setup(
 	name="""piaplib""",
-	version="""0.3.5""",
+	version="""0.3.7""",
 	description="""Beta for PiAP python tools""",
 	long_description=readme,
 	install_requires=requirements,
