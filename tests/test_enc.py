ec26b8ec02cfb27d93bc5c43ec7110bdcf67320d
reactive-firewall <reactive-firewall@users.noreply.github.com>
2020-02-27 13:45:38 -0800

    Fix for utf-8 vs ascii decryption testing

--- tests/test_enc.py
+++ tests/test_enc.py
@@ -361,8 +361,8 @@ class CryptoTestSuite(unittest.TestCase):
 			test_args = []
 			# temp_msg = str("""U2FsdGVkX1+dD6bFlND+Xa0bzNttrZfB5zYCp0mSEYfhMTpaM7U=""")
 			if sys.platform.startswith("linux"):
-				temp_msg = str("""U2FsdGVkX1+dD6bFlND+Xa0bzNttrZfB5zYCp0mSEYfhMTpaM7U=""")
-				# temp_msg = str("""U2FsdGVkX1/MMOdV6OYwAURQQg9b9K1AoVA0OCcanG9FjHk7gHk=""")
+				# temp_msg = str("""U2FsdGVkX1+dD6bFlND+Xa0bzNttrZfB5zYCp0mSEYfhMTpaM7U=""")
+				temp_msg = str("""U2FsdGVkX1/MMOdV6OYwAURQQg9b9K1AoVA0OCcanG9FjHk7gHk=""")
 				test_args = [
 					str("--unpack"),
 					str("--msg=\"{}\"").format(temp_msg),
